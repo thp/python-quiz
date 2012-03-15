2adeed9fa0bbcd8430812137228fe400a2beada0
Thomas Perl <m@thp.io>
2012-03-15 16:45:28 +0100

    Avoid overlong lines

--- 2012-03/pyquiz_2012-03_thp7_golf.py
+++ 2012-03/pyquiz_2012-03_thp7_golf.py
@@ -7,4 +7,5 @@ def c(w,v):
 for x in __import__('fileinput').input():
  try:x=int(x),'',1
  except:x=x,0,0
- print reduce(c,re.findall('(\d+)(\D+)','1000M900CM500D400CD100C90XC50L40XL10X9IX5V4IV1I'),x)[1]
+ print reduce(c,re.findall('(\d+)(\D+)',
+'1000M900CM500D400CD100C90XC50L40XL10X9IX5V4IV1I'),x)[1]
