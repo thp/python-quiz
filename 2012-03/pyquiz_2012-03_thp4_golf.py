28a3e807a98e83bc77d154be38367d6f5552c874
Thomas Perl <m@thp.io>
2012-03-15 15:40:45 +0100

    Improve notes; alternative import

--- 2012-03/pyquiz_2012-03_thp4_golf.py
+++ 2012-03/pyquiz_2012-03_thp4_golf.py
@@ -1,10 +1,12 @@
-import fileinput as f;a=[((1+4*(i%2))*10**(i/2),'IVXLCDM'[i])for i in range(7)];a+=[(x*c,y*c)for x,y in a[::2] for c in[2,3]]+[(a[x+1][0]-a[x/2*2][0],a[x/2*2][1]+a[x+1][1])for x in range(6)];a.sort(reverse=1)
-def b(x):
- try:return int(x),'',1
- except:return x,0,0
+a=[((1+4*(i%2))*10**(i/2),'IVXLCDM'[i])for i in range(7)];a+=[(x*c,y*c)for x,y in a[::2]for c in[2,3]]+[(a[x+1][0]-a[x/2*2][0],a[x/2*2][1]+a[x+1][1])for x in range(6)];a.sort(reverse=1)
 def c(w,v):
  x,e,d=w;g,h=v;i=len(h)
- if d and x>=g:x,e=x-g,e+h
- if not d and x[:i]==h:x,e=x[i:],e+g
+ if d:
+  if x>=g:x,e=x-g,e+h
+ else:
+  if x[:i]==h:x,e=x[i:],e+g
  return x,e,d
-for x in f.input():print reduce(c,a,b(x))[1]
+for x in __import__('fileinput').input():
+ try:x=int(x),'',1
+ except:x=x,0,0
+ print reduce(c,a,x)[1]
--- 2012-03/pyquiz_2012-03_thp_golf.py
+++ 2012-03/pyquiz_2012-03_thp_golf.py
@@ -1,6 +1,6 @@
-import fileinput as f;r=range;a=[((1+4*(i%2))*10**(i/2),'IVXLCDM'[i])for i in r(7)];a+=[(a[x+1][0]-a[x/2*2][0],a[x/2*2][1]+a[x+1][1])for x in r(6)]
+r=range;a=[((1+4*(i%2))*10**(i/2),'IVXLCDM'[i])for i in r(7)];a+=[(a[x+1][0]-a[x/2*2][0],a[x/2*2][1]+a[x+1][1])for x in r(6)]
 def c(n):
  x=n and int(i)or i
  for y,z in reversed(sorted(a)):
   while (n and x>=y)or(not n and x[:len(z)]==z):yield(n and z or y);x=(x-y)if n else x[len(z):]
-for i in filter(None,map(str.strip,f.input())):print reduce(lambda d,e:d+e,c(i.isdigit()))
+for i in filter(None,map(str.strip,__import__('fileinput').input())):print reduce(lambda d,e:d+e,c(i.isdigit()))
