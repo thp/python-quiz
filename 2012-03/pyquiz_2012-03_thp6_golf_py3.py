73fa3dee4d499a5973e706d4a84c01879cb5b6e4
Thomas Perl <m@thp.io>
2012-03-15 16:22:12 +0100

    Variant 6 (if/elif) + py3 support

--- 2012-03/pyquiz_2012-03_thp6_golf_py3.py
+++ 2012-03/pyquiz_2012-03_thp6_golf_py3.py
@@ -1,4 +1,4 @@
-import re
+import re;import functools as m
 def c(w,v):
  t,h,p=w;g,j=v;g=int(g);i=len(j)
  if p:
@@ -8,4 +8,4 @@ def c(w,v):
 for x in __import__('fileinput').input():
  try:x=int(x),'',1
  except:x=x,0,0
- print reduce(c,re.findall('(\d+)(\D+)','3000MMM2000MM1000M900CM500D400CD300CCC200CC100C90XC50L40XL30XXX20XX10X9IX5V4IV3III2II1I'),x)[1]
+ print(m.reduce(c,re.findall('(\d+)(\D+)','3000MMM2000MM1000M900CM500D400CD300CCC200CC100C90XC50L40XL30XXX20XX10X9IX5V4IV3III2II1I'),x)[1])
