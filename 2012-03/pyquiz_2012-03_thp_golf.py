c0d2124774d18cf5917a8e2985b116315fcbe69c
Thomas Perl <m@thp.io>
2012-03-15 15:17:37 +0100

    400C Quiz Golf

--- 2012-03/pyquiz_2012-03_thp_golf.py
+++ 2012-03/pyquiz_2012-03_thp_golf.py
@@ -2,5 +2,5 @@ import fileinput as f;r=range;a=[((1+4*(i%2))*10**(i/2),'IVXLCDM'[i]) for i in r
 def c(n):
  x=n and int(i)or i
  for y,z in reversed(sorted(a)):
-  while (n and x>=y)or(not n and x.startswith(z)):yield(n and z or y);x=(x-y)if n else x[len(z):]
+  while (n and x>=y)or(not n and x[:len(z)]==z):yield(n and z or y);x=(x-y)if n else x[len(z):]
 for i in filter(None,map(str.strip,f.input())):print reduce(lambda d,e:d+e,c(i.isdigit()))
