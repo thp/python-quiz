2a4f729ec48f9005a8f8a745a8dfea5673fe3e84
Thomas Perl <m@thp.io>
2012-03-15 15:18:27 +0100

    Stdin-only golf variant, 389C

---
+++ 2012-03/pyquiz_2012-03_thp_golf_stdin.py
@@ -0,0 +1,6 @@
+import sys;r=range;a=[((1+4*(i%2))*10**(i/2),'IVXLCDM'[i]) for i in r(7)];a+=[(a[x+1][0]-a[x/2*2][0],a[x/2*2][1]+a[x+1][1]) for x in r(6)]
+def c(n):
+ x=n and int(i)or i
+ for y,z in reversed(sorted(a)):
+  while (n and x>=y)or(not n and x[:len(z)]==z):yield(n and z or y);x=(x-y)if n else x[len(z):]
+for i in filter(None,map(str.strip,sys.stdin)):print reduce(lambda d,e:d+e,c(i.isdigit()))
