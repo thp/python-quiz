890164ddf7e2028646828b0efb67964404570f3b
Thomas Perl <m@thp.io>
2012-03-20 21:45:42 +0100

    Two more chars saved!

--- 2012-03/pyquiz_2012-03_thp8_dict.py
+++ 2012-03/pyquiz_2012-03_thp8_dict.py
@@ -3,6 +3,7 @@ def c(w,v):
  t,h=w;g,j=v;g=int(g)
  while t>=g:t,h=t-g,h+j
  return t,h
-d,k={},re.findall('(\d+)(\D+)','1000M900CM500D400CD100C90XC50L40XL10X9IX5V4IV1I')
-for i in range(1,4000):x=reduce(c,k,(i,''))[1];d[x]=i;d[str(i)]=x
+d={}
+k=re.findall('(\d+)(\D+)','1000M900CM500D400CD100C90XC50L40XL10X9IX5V4IV1I')
+for i in range(4000):x=reduce(c,k,(i,''))[1];d[x]=i;d[str(i)]=x
 for x in f.input():print d[x.strip()]
