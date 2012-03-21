1d9849f8509eff925486b45fb69f6c2e36e987b4
Thomas Perl <m@thp.io>
2012-03-21 17:40:34 +0100

    Updated and annotated summaries

--- 2012-03/profile/summary.py
+++ 2012-03/profile/summary.py
@@ -1,13 +1,20 @@
 
 import re
 import sys
+import os
 
 stats_re = r'(\d+) function calls.*in ([0-9.]+) seconds'
+
+def getlines(filename):
+    return len(open(filename).read().splitlines())
+
+def getchars(filename):
+    return len(open(filename).read())
 
 use_csv = ('-csv' in sys.argv)
 if use_csv:
     sys.argv.remove('-csv')
-    print 'Filename;Time (Seconds)'
+    print 'Filename;Time (Seconds);Calls;Lines;Chars'
 
 stats = []
 for filename in sys.argv[1:]:
@@ -19,6 +26,9 @@ stats.sort()
 
 for stat in stats:
     if use_csv:
-        print '%s;%f' % (stat[2], stat[0])
+        filename = '../%s.py' % (os.path.splitext(stat[2])[0])
+        lines = getlines(filename)
+        chars = getchars(filename)
+        print '%s;%f;%d;%d;%d' % (stat[2], stat[0], stat[1], lines, chars)
     else:
         print '   %10.3fs  %10d calls     %s' % stat
--- 2012-03/profile_short/summary.py
+++ 2012-03/profile_short/summary.py
@@ -7,7 +7,7 @@ stats_re = r'(\d+) function calls.*in ([0-9.]+) seconds'
 use_csv = ('-csv' in sys.argv)
 if use_csv:
     sys.argv.remove('-csv')
-    print 'Filename;Time (Seconds)'
+    print 'Filename;Time (Seconds);Calls'
 
 stats = []
 for filename in sys.argv[1:]:
@@ -19,6 +19,6 @@ stats.sort()
 
 for stat in stats:
     if use_csv:
-        print '%s;%f' % (stat[2], stat[0])
+        print '%s;%f;%d' % (stat[2], stat[0], stat[1])
     else:
         print '   %10.3fs  %10d calls     %s' % stat
