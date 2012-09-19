046e938a040470fa76c75e75b83b8eea512c2e8a
Thomas Perl <m@thp.io>
2012-09-19 20:55:33 +0200

    Add some explanations, plus (sic) negative support

--- 2012-09/number2string.py
+++ 2012-09/number2string.py
@@ -7,7 +7,33 @@
 
 import fileinput
 
+# '12341231.123123' -> split it at the '.'
+# ['12341231', '123123'] -> the first part needs padding
+# [pad('12341231'), '123123']
+# ^^^^               ^^^
+# '12.341.231'      '123123' -> ','.join()
+# 12.341.231,123123
+
+
+# '1234.123'
+# [(0, '1234'), (1, '123')]
+# '1234'
+# pad([(0, '1'), (1, '2'), (2, '3'), (3, '4')], 4) -> '1.234'
+# [(4, '1'), (3, '2'), (2, '3'), (1, '4')]
+# [(1, '1'), (0, '2'), (2, '3'), (1, '4')]
+#     1       .2        3            4
+
+# msep * False + c -> msep * 0 + c -> c
+# msep * True + c -> msep * 1 + c -> msep + c -> ".c"
+# the "i and" takes care of avoiding '123' -> '.123'
+
+# ['1.234', '123'] -> '1.234,123'
+
 def number2string(number, msep='.', sep=','):
+    # Support for "negative" numbers
+    if number.startswith('-'):
+        return '-' + number2string(number[1:], msep, sep)
+
     pad = lambda x, l: ''.join(msep*(i and (l-i)%3 == 0) + c for i, c in x)
     convert = lambda txt, pos: txt if pos else pad(enumerate(txt), len(txt))
     return sep.join(convert(a, i) for i, a in enumerate(number.split('.')))
--- 2012-09/test_number2string.py
+++ 2012-09/test_number2string.py
@@ -6,7 +6,9 @@ from nose.tools import assert_equal
 
 def check_result(x, y):
     assert_equal(number2string(x), y)
-    assert_equal(f(x), y)
+    if not x.startswith('-'):
+        # Trololol
+        assert_equal(f(x), y)
 
 def test_number2string():
     yield check_result, '10', '10'
@@ -15,6 +17,7 @@ def test_number2string():
     yield check_result, '1000000', '1.000.000'
     yield check_result, '1999995.99', '1.999.995,99'
     yield check_result, '1337.88888', '1.337,88888'
+    yield check_result, '-100000', '-100.000'
 
 def check_result_altsep(x, y):
     assert_equal(number2string(x, ',', '.'), y)
