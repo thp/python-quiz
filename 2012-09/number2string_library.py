90c07eef2cd149f7030bd6993368a9967799605b
Thomas Perl <m@thp.io>
2012-09-19 21:50:29 +0200

    Shorter (with apologies to hop who had the initial idea)

--- 2012-09/number2string_library.py
+++ 2012-09/number2string_library.py
@@ -1,1 +1,1 @@
-e=enumerate;f=lambda n,m='.',s=',':s.join(a if p else''.join(m*(i and(len(a)-i)%3==0)+c for i,c in e(a))for p,a in e(n.split('.')))
+e=enumerate;f=lambda n,m='.',s=',':s.join(a if p else''.join(m[i and(len(a)-i)%3:]+c for i,c in e(a))for p,a in e(n.split('.')))
